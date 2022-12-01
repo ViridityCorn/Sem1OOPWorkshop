353a5a9bfdfdbfd5e02d878743bf61613df50d37
jamiehoej <jamiehoej@gmail.com>
2022-12-01 21:50:06 +0100

    Implement Unit class

--- src/main/java/domain/Unit.java
+++ src/main/java/domain/Unit.java
@@ -5,14 +5,21 @@ import java.util.UUID;
 public abstract class Unit {
     private String name;
     private UUID id;
+    
+    public Unit(String name)
+    {
+        this.name = name;
+        this.id = UUID.randomUUID();
+    }
 
-    public UUID getId() {
-        return id;
+    public UUID getId()
+    {
+        return this.id;
     }
 
     public String getName()
     {
-
+        return this.name;
     }
 
     @Override
