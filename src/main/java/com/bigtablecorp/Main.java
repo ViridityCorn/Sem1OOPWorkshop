7eadec17faf12be262dca8968bf8c0797d4e0089
jamiehoej <jamiehoej@gmail.com>
2022-12-01 21:50:34 +0100

    crate TestUnit class for testing Unit

--- src/main/java/com/bigtablecorp/Main.java
+++ src/main/java/com/bigtablecorp/Main.java
@@ -1,9 +1,15 @@
 package com.bigtablecorp;
 
+import domain.TestUnit;
+
 public class Main {
 
     public int testing;
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        TestUnit boop = new TestUnit("hoopidihoop");
+
+        System.out.println(boop);
     }
 }
---
+++ src/main/java/domain/TestUnit.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class TestUnit extends Unit {
+    public TestUnit(String name) {
+        super(name);
+    }
+}
